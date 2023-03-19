1d64d1bc2174fca1a42594746fd5ce8516b92dd6
Sharjeel Yunus <sharjeel924@gmail.com>
2023-03-20 02:45:50 +0500

    leaderboard functionality

--- pages/leaderboard.tsx
+++ pages/leaderboard.tsx
@@ -1,25 +1,25 @@
 import React from 'react';
 import Layout from '../components/Layout';
 import LeaderboardCard from '../components/LeaderboardCard';
+import useGetLeaderboardData from '../hooks/useGetLeaderboard';
 
-type Props = {};
+const Leaderboard = () => {
+  const leaderboardData = useGetLeaderboardData();
 
-const Leaderboard = (props: Props) => {
   return (
     <Layout title='Leaderboard | ProgressPath'>
-      <div className='py-28 flex flex-col justify-center items-center min-h-screen bg-[#635985] text-white'>
+      <div className='py-28 flex flex-col items-center min-h-screen bg-[#635985] text-white'>
         <h1 className='font-bold text-2xl'>Leaderboard</h1>
         <div className='py-5'>
-          <LeaderboardCard />
-          <LeaderboardCard />
-          <LeaderboardCard />
-          <LeaderboardCard />
-          <LeaderboardCard />
-          <LeaderboardCard />
-          <LeaderboardCard />
-          <LeaderboardCard />
-          <LeaderboardCard />
-          <LeaderboardCard />
+          {leaderboardData.map((entry, index) => (
+            <LeaderboardCard
+              key={entry.authorId}
+              rank={index + 1}
+              authorId={entry.authorId}
+              points={entry.points}
+              completedTasks={entry.completedTasks}
+            />
+          ))}
         </div>
       </div>
     </Layout>
