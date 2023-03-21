291e6b3034f045e71fd14266057de004f04c5d86
Sharjeel Yunus <sharjeel924@gmail.com>
2023-03-21 06:00:04 +0500

    navigate to profiles via leaderboard

--- components/LeaderboardCard.tsx
+++ components/LeaderboardCard.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import React from 'react';
 import useGetTargetUser from '../hooks/useGetTargetUser';
 
@@ -12,27 +13,56 @@ const LeaderboardCard = ({ rank, authorId, points, completedTasks }: Props) => {
   const targetUser = useGetTargetUser(authorId);
 
   return (
-    <div className='flex items-center justify-between bg-[#393053] text-white px-5 py-3 rounded-2xl w-[350px] lg:w-[500px] mt-3'>
-      <div className='flex items-center gap-3'>
-        <span className='font-bold text-xl'>{rank}. </span>
-        <img
-          src={
-            targetUser?.photoURL
-              ? targetUser?.photoURL
-              : '/blank-profile-picture.svg'
-          }
-          alt=''
-          className='w-10 h-10 rounded-full'
-        />
-        <div>
-          <h2 className='font-bold'>{targetUser?.name}</h2>
-          <span className='text-sm'>Completed {completedTasks} tasks</span>
+    <>
+      {targetUser.username ? (
+        <Link
+          href={`/user/${targetUser?.username}`}
+          className='flex items-center justify-between bg-[#393053] text-white px-5 py-3 rounded-2xl w-[350px] lg:w-[500px] mt-3'
+        >
+          <div className='flex items-center gap-3'>
+            <span className='font-bold text-xl'>{rank}. </span>
+            <img
+              src={
+                targetUser?.photoURL
+                  ? targetUser?.photoURL
+                  : '/blank-profile-picture.svg'
+              }
+              alt=''
+              className='w-10 h-10 rounded-full'
+            />
+            <div>
+              <h2 className='font-bold'>{targetUser?.name}</h2>
+              <span className='text-sm'>Completed {completedTasks} tasks</span>
+            </div>
+          </div>
+          <div className='border-l'>
+            <p className='font-bold text-lg ml-3'>{points}pt</p>
+          </div>
+        </Link>
+      ) : (
+        <div className='flex items-center justify-between bg-[#393053] text-white px-5 py-3 rounded-2xl w-[350px] lg:w-[500px] mt-3'>
+          <div className='flex items-center gap-3'>
+            <span className='font-bold text-xl'>{rank}. </span>
+            <img
+              src={
+                targetUser?.photoURL
+                  ? targetUser?.photoURL
+                  : '/blank-profile-picture.svg'
+              }
+              alt=''
+              className='w-10 h-10 rounded-full'
+            />
+            <div>
+              <h2 className='font-bold'>{targetUser?.name}</h2>
+              <span className='text-sm'>Completed {completedTasks} tasks</span>
+            </div>
+          </div>
+          <div className='border-l'>
+            <p className='font-bold text-lg ml-3'>{points}pt</p>
+          </div>
         </div>
-      </div>
-      <div className='border-l'>
-        <p className='font-bold text-lg ml-3'>{points}pt</p>
-      </div>
-    </div>
+      )}
+    </>
   );
 };
 
