5aee2b3e585d9f6b869c8829a69f30367d395340
Sharjeel Yunus <sharjeel924@gmail.com>
2023-06-05 05:31:15 +0500

    leaderboard fix

--- hooks/useGetLeaderboard.tsx
+++ hooks/useGetLeaderboard.tsx
@@ -1,59 +1,54 @@
 import { useEffect, useState } from 'react';
 import { LeaderboardEntry } from '../interfaces';
-import { getCache, setCache } from '../utils/cache';
-
-interface PaginationData {
-  currentPage: number;
-  itemsPerPage: number;
-}
+// import { getCache, setCache } from '../utils/cache';
 
 export default function useGetLeaderboardData(
   initialPage: number = 1,
   itemsPerPage: number = 10
-): [LeaderboardEntry[], PaginationData, (pageNumber: number) => void] {
+): [LeaderboardEntry[]] {
   const [leaderboardData, setLeaderboardData] = useState<LeaderboardEntry[]>(
     []
   );
-  const [paginationData, setPaginationData] = useState<PaginationData>({
-    currentPage: initialPage,
-    itemsPerPage,
-  });
+  // const [paginationData, setPaginationData] = useState<PaginationData>({
+  //   currentPage: initialPage,
+  //   itemsPerPage,
+  // });
 
   const fetchLeaderboardData = async () => {
     try {
-      const response = await fetch(`/api/leaderboard?page=${initialPage}`);
+      const response = await fetch(`/api/leaderboard`);
       const data = await response.json();
       setLeaderboardData(data.leaderboard);
-      setCache('leaderboardData', data.leaderboard); // cache the fetched data
+      // setCache('leaderboardData', data.leaderboard); // cache the fetched data
     } catch (error) {
       throw new Error('Failed to fetch leaderboard data');
     }
   };
 
-  const changePage = (pageNumber: number) => {
-    setPaginationData((prevPaginationData) => ({
-      ...prevPaginationData,
-      currentPage: pageNumber,
-    }));
-  };
+  // const changePage = (pageNumber: number) => {
+  //   setPaginationData((prevPaginationData) => ({
+  //     ...prevPaginationData,
+  //     currentPage: pageNumber,
+  //   }));
+  // };
 
   useEffect(() => {
-    const cachedData = getCache<LeaderboardEntry[]>('leaderboardData'); // check if there is cached data
-    if (cachedData) {
-      setLeaderboardData(cachedData); // set the cached data if available
-    } else {
-      fetchLeaderboardData().catch((error) => console.error(error.message)); // fetch data if there is no cached data
-    }
+    // const cachedData = getCache<LeaderboardEntry[]>('leaderboardData'); // check if there is cached data
+    // if (cachedData) {
+    //   setLeaderboardData(cachedData); // set the cached data if available
+    // } else {
+    fetchLeaderboardData().catch((error) => console.error(error.message)); // fetch data if there is no cached data
+    // }
   }, []);
 
   // Calculate the start and end index of the items to display based on the current page and items per page
-  const startIndex =
-    (paginationData.currentPage - 1) * paginationData.itemsPerPage;
-  const endIndex = startIndex + paginationData.itemsPerPage;
+  // const startIndex =
+  //   (paginationData.currentPage - 1) * paginationData.itemsPerPage;
+  // const endIndex = startIndex + paginationData.itemsPerPage;
 
   return [
-    leaderboardData.slice(startIndex, endIndex),
-    paginationData,
-    changePage,
+    leaderboardData,
+    // paginationData,
+    // changePage,
   ];
 }
