6c024c415e74bd74db08b8d9181286c2b4eada30
Sharjeel Yunus <sharjeel924@gmail.com>
2023-08-24 00:32:02 +0500

    update mentor view

--- pages/[...slug].tsx
+++ pages/[...slug].tsx
@@ -7,6 +7,9 @@ import { useAuth } from '../context/AuthContext';
 import useGetAllTasks from '../hooks/useGetTasks';
 import AddTask from '../components/Dashboard/AddTask';
 import Loading from '../src/shared/components/Loading';
+import { doc, updateDoc } from 'firebase/firestore';
+import { db } from '../config/firebase';
+import { toast } from 'react-hot-toast';
 
 const Training = () => {
   const router = useRouter();
@@ -25,20 +28,39 @@ const Training = () => {
     return <Loading />;
   }
 
+  const handleRequestPublishTrack = async () => {
+    await updateDoc(doc(db, 'trainings', track?.id), {
+      trackStatus: 'in-review',
+    });
+
+    toast.success('Your track has been submitted for review');
+
+    router.push('/');
+  };
+
   return (
     <Layout title={`${track?.name} | ProgressPath`}>
       <div className='flex justify-center bg-gray-700 py-28 min-h-screen'>
         <div className='flex flex-col w-11/12 lg:w-3/4'>
           <div>
-            <div className='flex justify-end'>
-              {loggedInUser?.mentorTracks?.includes(track?.id) &&
-                tasks.length > 10 &&
-                track.trackStatus == 'pending' && (
-                  <button className='text-white bg-gray-900 px-10 py-4 rounded-xl'>
+            {loggedInUser?.mentorTracks?.includes(track?.id) &&
+              tasks.length > 10 &&
+              track?.trackStatus == 'pending' && (
+                <div className='flex justify-end'>
+                  <button
+                    onClick={handleRequestPublishTrack}
+                    className='text-white bg-gray-900 px-10 py-4 rounded-xl'
+                  >
                     Publish Track
                   </button>
-                )}
-            </div>
+                </div>
+              )}
+
+            {track?.trackStatus !== 'Published' && (
+              <p className='text-black font-bold text-center bg-white p-3 my-10 rounded-md'>
+                This track is still in development. You'll be able to publish it once you've added at least 10 tasks.
+              </p>
+            )}
             <h1 className='text-4xl font-bold text-white text-center'>
               {track?.name}
             </h1>
